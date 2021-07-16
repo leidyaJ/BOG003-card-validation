0a8ba323c1f8faf62551ef5a65d1e049b333b6ac
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-07-16 12:01:52 -0500

    mostrar y coualtar pantallas deacuerdo a la funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import validator from './validator.js';
+//console.log(validator);
+document.getElementById("buy").addEventListener ("click",() => {
+    document.getElementById("pag2").style.display= "block";
+    document.getElementById("pag1").style.display= "none";
+    document.getElementById("correctVal").style.display= "none";
+    document.getElementById("incorrectVal").style.display= "none";
+  });
 
-console.log(validator);
+    const btnValidar=document.getElementById("validButton");
+    btnValidar.addEventListener("click", ()=>{
+    const creditCardNumber=document.getElementById("cardNumber").value;
+    let re=validator.isValid(creditCardNumber);
+    
+    if (re){
+        document.getElementById("pag2").style.display= "none";
+        document.getElementById("correctVal").style.display= "block";
+        validator.maskify(creditCardNumber);
+        //document.getElementById.write('validación exitosa!!');
+
+        
+        
+    }else{
+        document.getElementById("pag2").style.display= "none";
+        document.getElementById("incorrectVal").style.display= "block";
+        //document.write('validación incorrecta!!');
+       
+
+    }
+});
+
+
+
+/* 
+    document.getElementById("pag2").style.display= "none";
+    document.getElementById("pag1").style.display= "none";
+    document.getElementById("correctVal").style.display= "none";
+    document.getElementById("incorrectVal").style.display= "none";});
+
+//    button funtion for input validation
+
+    const btnValidar=document.getElementById("validButton");
+    btnValidar.addEventListener("click", ()=>{
+    const creditCardNumber=document.getElementById("cardNumber").value;
+    let re=validator.isValid(creditCardNumber);
+    
+    if (re){
+        console.log('arbol'+ re);
+        
+    }else{
+        console.log('flor'+ re);
+
+    } */
+
+    
+//document.getElementById("validButton").addEventListener("click", ()=>{
+        
+
+    
+    
+   // validator.maskify(creditCardNumber);
+
+
+
+
+ /* document.getElementById("validButton").addEventListener("click", ()=>{
+    document.getElementById("pag2").style.display= "none";
+    document.getElementById("pag1").style.display= "none";
+    document.getElementById("correctVal").style.display= "block";
+    document.getElementById("incorrectVal").style.display= "none";}) */
